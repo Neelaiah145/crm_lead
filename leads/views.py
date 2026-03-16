99f817d1b5cebe711bf2b1a66e55bb9dcc743032
Neelaiah145 <rusumneelaiah@gmail.com>
2026-03-16 15:07:32 +0530

    chnages in views

--- leads/views.py
+++ leads/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render,redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Contact_lead
 from rest_framework.renderers import TemplateHTMLRenderer
+from django.contrib import messages
+
+from .models import Contact_lead
 from .serializers import Contact_leadSerializer
+
 
 class ContactList(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = "leads_page.html"
+
     def get(self, request):
         contacts = Contact_lead.objects.all()
         serializer = Contact_leadSerializer(contacts, many=True)
-        return Response({"contacts":serializer.data})
-   
+        return Response(serializer.data)
 
     def post(self, request):
         serializer = Contact_leadSerializer(data=request.data)
@@ -24,22 +25,36 @@ class ContactList(APIView):
             return Response(serializer.data)
 
         return Response(serializer.errors)
- 
+
+
+
+
+
+
+
+
 
 class DeleteLeadManagement(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "leads_page.html"
 
     def get(self, request):
 
         delete_id = request.GET.get("delete_id")
+        selected_ids = request.GET.get("selected_ids")
 
         if delete_id:
             Contact_lead.objects.filter(id=delete_id).delete()
-            return redirect("contact_lead/")
+            messages.success(request, "Lead deleted successfully")
 
-        selected_ids = request.GET.get("selected_ids")
-
-        if selected_ids:
+        elif selected_ids:
             ids = selected_ids.split(",")
             Contact_lead.objects.filter(id__in=ids).delete()
+            messages.success(request, "Selected leads deleted successfully")
 
-        return redirect("contact_lead")
+        contacts = Contact_lead.objects.all()
+        serializer = Contact_leadSerializer(contacts, many=True)
+
+        return Response({
+            "contacts": serializer.data
+        })
