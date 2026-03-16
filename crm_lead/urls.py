25f42eef03de719240a8470cbf7b34f57f3015ff
Neelaiah145 <rusumneelaiah@gmail.com>
2026-03-16 12:27:59 +0530

    create and delete only modified

--- crm_lead/urls.py
+++ crm_lead/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from leads import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.ContactList.as_view(),name="contact_lead"),
+    path("delete_leads/", views.DeleteLeadManagement.as_view(), name="delete_leads")
 ]
+
+
--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact_lead(models.Model):
+        STATUS_CHOICES = (
+        ('new', 'New'),
+        ('in_progress', 'In Progress'),
+        ('interest', 'interest'),
+        ('closed', 'closed'),
+    )
+        name = models.CharField(max_length=100)
+        phone_number = models.CharField( max_length=15, blank=True, null=True)
+        email = models.EmailField()
+        message = models.TextField()
+        status = models.CharField(max_length=20,choices=STATUS_CHOICES,default='new')
+        created_at = models.DateTimeField(auto_now_add=True)
+        
+        
